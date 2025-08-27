11695066c7e3ede663042855aec8f398f4d8b432
aleksandr <krainukovaleksandr1@gmail.com>
2025-08-27 12:06:36 +0300

    settings in process

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,23 +1,47 @@
-import { Button, Divider, Layout } from "antd";
+import { Button, Checkbox, Divider, Layout, Select, Switch } from "antd";
 import styles from "./Settings.module.scss";
 import React from "react";
 import { Content } from "antd/es/layout/layout";
 import Title from "antd/es/typography/Title";
 import Paragraph from "antd/es/typography/Paragraph";
 import Logout from "@assets/Icon trailing.svg?react";
+import { Tabs } from "antd";
+import { TabsProps } from "antd";
+import * as Tab from "../Tabs";
+
+
+const items: TabsProps["items"] = [
+  {
+    key: "1",
+    label: "General",
+    children: <Tab.GeneralTab />,
+  },
+  {
+    key: "2",
+    label: "Account",
+    children: <Tab.AccountTab />,
+  },
+  {
+    key: "4",
+    label: "Notification",
+    children: <Tab.NotificationTab />,
+  },
+  
+];
 
 const Settings = () => {
+
   return (
     <Layout className={styles.detailLayout}>
       <Content>
         <div className={styles.contactInfo}>
           <div className={styles.header}>
-            <div className={styles.avatar}>u</div>
+            <div className={styles.avatar}>S</div>
             <div>
               <Title level={3} style={{ marginBottom: 0 }}>
-                You
+                Sylvia Reyes
               </Title>
-              <Paragraph style={{ marginBottom: 0 }}>899999999</Paragraph>
+              <Paragraph style={{ marginBottom: 0 }}>+44656548600</Paragraph>
             </div>
             <div className={styles.headerActions}>
               <Button
@@ -32,9 +56,7 @@ const Settings = () => {
             </div>
           </div>
           <Divider />
-          <div className={styles.overview}>
-            <Title level={5}>General</Title>
-          </div>
+          <Tabs items={items} className={styles.tabs} />
         </div>
       </Content>
     </Layout>
---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,90 @@
+import { Button, Checkbox, Input, Select, Switch } from "antd";
+import Title from "antd/es/typography/Title";
+import styles from './Tabs.module.scss'
+import Paragraph from "antd/es/typography/Paragraph";
+
+export const GeneralTab: React.FC = () => {
+    return (
+      <div className={styles.infoBlock}>
+        <Title level={5} style={{ margin: 0 }}>General</Title>
+        <div>
+          <b>Login</b>
+          <div>
+            <Switch defaultChecked /> <span style={{ marginLeft: 8 }}>On</span>
+          </div>
+        </div>
+        <div>
+          <b>Language</b>
+          <Select defaultValue="default" style={{ width: 240 }}
+            options={[
+              { value: "default", label: "Default" },
+              { value: "en", label: "English" },
+              { value: "ru", label: "Русский" },
+            ]}
+          />
+        </div>
+        <div>
+          <b>Password</b>
+          <div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
+            <Checkbox defaultChecked>Photo</Checkbox>
+            <Checkbox defaultChecked>Audio</Checkbox>
+            <Checkbox defaultChecked>Video</Checkbox>
+            <Checkbox>Document</Checkbox>
+          </div>
+        </div>
+        <div>
+          <b>Messages</b>
+          <Button size="small">Archive all</Button>
+        </div>
+      </div>
+    );
+  };
+
+  export const AccountTab: React.FC = () => {
+    return (
+<div className={styles.infoBlock}>
+      <Title level={5} className={styles.title}>
+        Account
+      </Title>
+
+      <div className={styles.field}>
+        <label className={styles.label}>Username</label>
+        <Input className={styles.input} placeholder="Sylvia Reyes"  />
+      </div>
+
+      <div className={styles.field}>
+        <label className={styles.label}>Birthday</label>
+        <Input className={styles.input} placeholder="19/10/1994"  />
+      </div>
+
+      <div className={styles.field}>
+        <label className={styles.label}>Email</label>
+        <Input className={styles.input} placeholder="sylvia@mercure.studio"  />
+      </div>
+
+      <div className={styles.field}>
+        <label className={styles.label}>Password</label>
+        <Input.Password className={styles.input} placeholder="********"  />
+      </div>
+    </div>
+    );
+  };
+
+
+    export const NotificationTab: React.FC = () => {
+    return (
+      <div className={styles.infoBlock}>
+        <Title level={5} style={{ margin: 0 }}>General</Title>
+        <Paragraph>Receive notifications for comments, tags, change request and any new activity.</Paragraph>
+        <div>
+          <b>Communication</b>
+          <div>
+            <Switch defaultChecked /> <span style={{ marginLeft: 8 }}>Push</span>
+          </div>
+        </div>
+    
+      </div>
+    );
+}
+
+
